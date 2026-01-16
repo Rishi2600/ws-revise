6dfa652f4680ac5737b6738294873768a49dd256
Rishi2600 <rishiraj6177@gmail.com>
2026-01-17 00:03:12 +0530

    express protocol: broadcast

--- src/index.ts
+++ src/index.ts
@@ -6,14 +6,28 @@ const port: number = 8080;
 
 const wss = new WebSocketServer({ port });
 
+app.get('/', (req, res) => {
+  console.log(`${(new Date())}, recieved a ${req.method} request from ${req.url}`)
+  res.json({
+    response: "Hey there"
+  })
+})
+
 wss.on('connection', function connection (socket) {
   socket.on ('error', console.error)
 
   socket.on('message', function message(data) {
-    console.log(data)
-    const message = socket.send(data)
-    return message
+    wss.clients.forEach(function each(client) {
+      if (client.readyState === WebSocket.OPEN) {
+        console.log(data)
+      const message = socket.send(data)
+      client.send(data)
+      }
+    })
+    
   })
+
+  socket.send("this is from server, you have the connection!!!")
 
 })
 
