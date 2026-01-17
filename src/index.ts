955880ed2744b7c056bcf612dfd458c44ba03c5a
Rishi2600 <rishiraj6177@gmail.com>
2026-01-17 23:51:51 +0530

    using express, I created a broadcasting system that sends the broadcasts the message sent to every client connected to that very instance (a chat app basicall)

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { WebSocketServer } from "ws";
+import WebSocket, { WebSocketServer } from "ws";
 import express from 'express';
 
 const app = express();
@@ -15,17 +15,15 @@ const wss = new WebSocketServer({port: 8080})
 wss.on('connection', (socket) => {
   socket.on('error', console.error)
 
+  //for broadcasting - first, we check where the handler is (here, it is at the message), and then we iterate to all the clients, followed by forwarding them the message.
   socket.on('message', (data, isBinary) => {
-
-    const message = data.toString();
-
-    if (message === "ping") {
-      socket.send("pong", {
-        binary: isBinary
-      })
-    } else {
-      socket.send("send ping to get back pong")
-    }
+    wss.clients.forEach((client) => {
+      if (client.readyState === WebSocket.OPEN) {
+        client.send(data, {
+          binary: isBinary
+        })
+      }
+    })
   })
 
   socket.send("This message is to confirm the connection is estableished with the wss")
